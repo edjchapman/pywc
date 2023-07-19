a1d70525fd2da619aa1a14f025b20610f6d23c5a
Ed <edchapman88@gmail.com>
2023-07-19 01:25:54 +0100

    Rename project from wc to pywc

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 
 setuptools.setup(
-    name="wc",
-    version="0.0.1",
+    name="pywc",
+    version="0.0.2",
     author="Ed Chapman",
     author_email="ed@edchapman.co.uk",
     license="MIT",
     description="Python implementation of the UNIX wc (Word Count) utility",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/edjchapman/wc",
-    py_modules=["wc", "app"],
+    url="https://github.com/edjchapman/pywc",
+    py_modules=["pywc", "app"],
     packages=setuptools.find_packages(),
     install_requires=[requirements],
     python_requires=">=3.7",
@@ -25,6 +25,6 @@ setuptools.setup(
     ],
     entry_points="""
         [console_scripts]
-        pywc=wc:cli
+        pywc=pywc:cli
     """,
 )
